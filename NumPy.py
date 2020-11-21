a3ba8fdd550b4388c81dfef731a1677a1703ed2e
HR <masluzsolar@gmail.com>
2020-11-21 16:55:43 -0600

    Cambio commit Version 2

--- NumPy.py
+++ NumPy.py
@@ -10,3 +10,6 @@ print("1> Array Shape is: ", firstArray.shape)
 print("2>. Array dimensions are ", firstArray.ndim)
 print("3>. Length of each element of array in bytes is ", firstArray.itemsize)
 print("\n")
+i=4
+print(i)
+print("\ncommit2")
